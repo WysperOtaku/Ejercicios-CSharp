9ca34963accac053cc94fc4a8b6b90c401b95d64
Wysper <85033182+WysperOtaku@users.noreply.github.com>
2023-11-05 19:41:26 +0100

    Add files via upload

---
+++ FizzBuzz - 01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codember01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i <= 100; i++){
+
+                if(i % 3 == 0 && i % 5 == 0){
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if (i % 3 == 0){
+                    Console.WriteLine("fizz");
+                }
+                else if(i % 5 == 0){
+                    Console.WriteLine("buzz");
+                }
+                else {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
